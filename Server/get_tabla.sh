9ab40bfbf8cce63c7454f6a8a97b21d975b73318
ltch23 <luistong23@gmail.com>
2018-07-10 14:35:02 -0500

    delete

--- Server/get_tabla.sh
+++
@@ -1,5 +0,0 @@
-curl 127.0.0.1:8000/ip_port.php > html.txt
-g++ parser.cpp
-./a.out
-echo "tabla "
-cat table.txt	
--- Server/llenar_tabla.sh
+++
@@ -1,1 +0,0 @@
-curl -d user="$1" -d ip="$2" -d port="$3" -d submit="Register" http://127.0.0.1:8000/ip_port.php
